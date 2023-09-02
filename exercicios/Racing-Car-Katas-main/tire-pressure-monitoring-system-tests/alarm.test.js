3b5403d4661fb6a7528348ec3b2a32c42ca4b5bf
Jessica Rafaeli Baron <jessica.baron@penseavanti.com.br>
2023-09-02 09:34:08 -0300

    feat: Alarm test

---
+++ exercicios/Racing-Car-Katas-main/tire-pressure-monitoring-system-tests/alarm.test.js
@@ -0,0 +1,14 @@
+/* globals describe, it */
+var jest = require("jest");
+jest.should();
+
+var Alarm = require("../tire-pressure-monitoring-system/alarm.js");
+
+describe("Tyre Pressure Monitoring System", function () {
+  describe("Alarm", function () {
+    it("foo", function () {
+      var alarm = new Alarm();
+      alarm.alarmOn().should.equal(false);
+    });
+  });
+});
